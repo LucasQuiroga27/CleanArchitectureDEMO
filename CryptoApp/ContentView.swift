c6c8d68636a796f0d2a607223b718584e71ae9e9
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-10-26 17:56:10 -0500

    primer commit, creando el proyecto

---
+++ CryptoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Lucas Quiroga on 26/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
