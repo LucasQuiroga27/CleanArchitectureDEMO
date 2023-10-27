f9fce4f6fa27878f93844e090627d9aecc3c155e
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-10-26 21:13:43 -0500

    configurando caso de uso

---
+++ CryptoApp/Domain/Use cases/GetGlobalCryptoList.swift
@@ -0,0 +1,41 @@
+//
+//  GetGlobalCryptoList.swift
+//  CryptoApp
+//
+//  Created by Lucas Quiroga on 26/10/23.
+//
+
+import Foundation
+
+enum CryptocurrencyDomainError: Error {
+    case generic
+}
+
+protocol GlobalCryptoListRepositoryType {
+    func getGlobalCryptoList() async -> Result<[Cryptocurrency], CryptocurrencyDomainError>
+}
+
+class GetGlobalCryptoList {
+    
+    private let repository: GlobalCryptoListRepositoryType
+    
+    init(repository: GlobalCryptoListRepositoryType) {
+        self.repository = repository
+    }
+    
+    func execute() async -> Result<[Cryptocurrency], CryptocurrencyDomainError>{
+        let result = await repository.getGlobalCryptoList()
+        
+        guard let cryptoList = try? result.get() else {
+            guard case .failure(let error) = result else {
+                return .failure(.generic)
+            }
+            
+            return .failure(error)
+
+        }
+        
+        return .success(cryptoList.sorted { $0.marketCap > $1.marketCap })
+        
+    }
+}
